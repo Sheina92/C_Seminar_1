8d81a986996a127fd423d3323161d817d3dc0ea1
Ирина <ira.sheina.92@gmail.com>
2023-10-09 13:51:00 +0300

    добавлено решение Task_004

---
+++ Task_004/Program.cs
@@ -0,0 +1,8 @@
+﻿// Программа, которая на вход принимает число (number), а на выходе выводит все чётные числа от 1 до number (включительно), разделеные знаком табуляции.
+Console.WriteLine ("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 2; i <= number; i+=2)  
+{
+    Console.Write (i + "\t");
+}
